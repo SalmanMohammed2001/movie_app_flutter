815ae59a956f6fac34bc618487fdea18eb369fe0
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-05-20 02:49:31 +0530

    splash screen to homepage route setup

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_app/screen/home_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,6 +12,18 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    /*Timer(Duration(seconds: 3),(){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => HomePage()));
+    });*/
+    Future.delayed(Duration(seconds:  3),() =>{
+    Navigator.push(context,MaterialPageRoute(builder: (context) => HomePage()))
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
@@ -37,4 +52,6 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
+
+
 }
