734c5e4bbd908716abae5d7b6108dafb7d8a229b
salman <salmannisthar123@gmail.com>
2024-07-07 05:23:39 +0530

    create movie model

---
+++ lib/model/movie_model.dart
@@ -0,0 +1,12 @@
+class MovieModel {
+  String title;
+  String overview;
+  String backdropPath;
+  String posterPath;
+
+  MovieModel(
+      {required this.title,
+      required this.overview,
+      required this.backdropPath,
+      required this.posterPath});
+}
