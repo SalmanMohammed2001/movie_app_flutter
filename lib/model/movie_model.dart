a2fa468e98e4366cba95111b53f09a74b63e91fa
salman <salmannisthar123@gmail.com>
2024-07-07 05:34:13 +0530

    create movie model factory

--- lib/model/movie_model.dart
+++ lib/model/movie_model.dart
@@ -4,9 +4,16 @@ class MovieModel {
   String backdropPath;
   String posterPath;
 
-  MovieModel(
-      {required this.title,
-      required this.overview,
-      required this.backdropPath,
-      required this.posterPath});
+  MovieModel({required this.title,
+    required this.overview,
+    required this.backdropPath,
+    required this.posterPath});
+
+  factory MovieModel.fromJson(Map<String, dynamic> json){
+    return MovieModel(title: json['title'],
+        overview: json['overview'],
+        backdropPath: json['backdrop_path'],
+        posterPath: json['poster_path']
+    );
+  }
 }
