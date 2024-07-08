d7170861341ad34dcbfaae7b1f1132f1393c985b
salman <salmannisthar123@gmail.com>
2024-07-08 12:49:03 +0530

    grop headingText,divider,futureBuilder

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -149,97 +149,103 @@ class _HomePageState extends State<HomePage> {
               const SizedBox(
                 height: 10,
               ),
-              const Text(
-                "Now Playing",
-                style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-              const Divider(),
-              FutureBuilder(
-                future: ApiServices().getNowPlayingMovies(),
-                builder: (context,snapshot) {
-                  if(snapshot.connectionState == ConnectionState.waiting){
-                    return const CircularProgressIndicator();
-                  }
-                  if(snapshot.hasError){
-                    return const Text("Something went Wrong");
-                  }
-                List<MovieModel> movies=snapshot.data!;
-                  return SizedBox(
-                    height: size.height * 0.15,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: movies.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SizedBox(
-                            width: size.width * 0.60,
-                            child: (Container(
-                              width: size.width * 0.50,
-                              height: size.height * 0.15,
-                              decoration: BoxDecoration(
-                                  color: Colors.grey.shade900,
-                                  borderRadius: BorderRadius.circular(15)),
-                              child: Row(
-                                children: [
-                                  ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: Image.network(
-                                        movies[index].posterPath)),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                      movies[index].title.substring(0,10), // Handling overflow with ellipsis (... at the end)
-                                        style:
-                                            TextStyle(color: Colors.grey.shade300),
-                                      ),
-                                      Container(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 8),
-                                        decoration: BoxDecoration(
-                                            color:
-                                                Colors.deepOrange.withOpacity(0.5),
-                                            borderRadius:
-                                                BorderRadius.circular(20)),
-                                        child: Row(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Now Playing",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Divider(),
+                  FutureBuilder(
+                      future: ApiServices().getNowPlayingMovies(),
+                      builder: (context,snapshot) {
+                        if(snapshot.connectionState == ConnectionState.waiting){
+                          return const CircularProgressIndicator();
+                        }
+                        if(snapshot.hasError){
+                          return const Text("Something went Wrong");
+                        }
+                        List<MovieModel> movies=snapshot.data!;
+                        return SizedBox(
+                          height: size.height * 0.15,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: movies.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SizedBox(
+                                  width: size.width * 0.60,
+                                  child: (Container(
+                                    width: size.width * 0.50,
+                                    height: size.height * 0.15,
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey.shade900,
+                                        borderRadius: BorderRadius.circular(15)),
+                                    child: Row(
+                                      children: [
+                                        ClipRRect(
+                                            borderRadius: BorderRadius.circular(10),
+                                            child: Image.network(
+                                                movies[index].posterPath)),
+                                        const SizedBox(
+                                          width: 5,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
                                           children: [
                                             Text(
-                                              movies[index].voteAverage.toString().substring(0, 3),
-                                              style: const TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 13,
-                                                  fontWeight: FontWeight.bold),
+                                              movies[index].title.substring(0,10), // Handling overflow with ellipsis (... at the end)
+                                              style:
+                                              TextStyle(color: Colors.grey.shade300),
                                             ),
-                                            const SizedBox(
-                                              width: 5,
+                                            Container(
+                                              padding: const EdgeInsets.symmetric(
+                                                  horizontal: 8),
+                                              decoration: BoxDecoration(
+                                                  color:
+                                                  Colors.deepOrange.withOpacity(0.5),
+                                                  borderRadius:
+                                                  BorderRadius.circular(20)),
+                                              child: Row(
+                                                children: [
+                                                  Text(
+                                                    movies[index].voteAverage.toString().substring(0, 3),
+                                                    style: const TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 13,
+                                                        fontWeight: FontWeight.bold),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Icon(
+                                                    Icons.star,
+                                                    color: Colors.yellow.shade600,
+                                                    size: 12,
+                                                  )
+                                                ],
+                                              ),
                                             ),
-                                            Icon(
-                                              Icons.star,
-                                              color: Colors.yellow.shade600,
-                                              size: 12,
-                                            )
                                           ],
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            )),
+                                        )
+                                      ],
+                                    ),
+                                  )),
+                                ),
+                              );
+                            },
                           ),
                         );
-                      },
-                    ),
-                  );
-                }
-              )
+                      }
+                  )
+                ],
+              ),
+
             ],
           ),
         ),
