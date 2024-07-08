7e73a77ac184e19a87379e80bf34f113af3929b1
salman <salmannisthar123@gmail.com>
2024-07-08 14:20:05 +0530

    moviePage backbutton implementation

--- lib/screen/movie_view/movie_view.dart
+++ lib/screen/movie_view/movie_view.dart
@@ -1,11 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:movie_app/model/movie_model.dart';
 
 class MovieView extends StatefulWidget {
-  const MovieView({
-    super.key,
-    required this.movie
-  });
+  const MovieView({super.key, required this.movie});
 
   final MovieModel movie;
 
@@ -16,24 +15,35 @@ class MovieView extends StatefulWidget {
 class _MovieViewState extends State<MovieView> {
   @override
   Widget build(BuildContext context) {
-    final size =MediaQuery.sizeOf(context);
-    return  Scaffold(
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              width: size.width,
-              height: size.height *0.25,
-              decoration: BoxDecoration(
+        child: Column(children: [
+          Container(
+            width: size.width,
+            height: size.height * 0.25,
+            decoration: BoxDecoration(
                 color: Colors.grey.shade900,
-                image:  DecorationImage(image: NetworkImage(
-                  widget.movie.backdropPath
-                ))
-              ),
-            )
-          ]
-        ),
+                image: DecorationImage(
+                    image: NetworkImage(widget.movie.backdropPath),
+                    fit: BoxFit.cover)),
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 8,
+                    left: 8,
+                    child: BackButton(
+
+                      color: Colors.white,
+                      style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                              const Color(0xFF000000).withOpacity(0.5))),
+                    ))
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
